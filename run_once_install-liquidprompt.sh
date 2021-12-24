db39c1b8a29654d0bf14ec5ac7edf9818a792d54
gde82 <1549599+gde82@users.noreply.github.com>
2021-12-24 11:55:34 +0100

    test condition on liquidprompt directory no need parenthese

--- run_once_install-liquidprompt.sh
+++ run_once_install-liquidprompt.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-if [[ ! -d "~/.local/liquidprompt" && "command -v git" ]]
+if [[ ! -d ~/.local/liquidprompt && `command -v git` ]]
     then
+        echo "create liquidprompt directory"
         mkdir -p ~/.local/
         git clone --branch stable https://github.com/nojhan/liquidprompt.git ~/.local/liquidprompt 
 fi
