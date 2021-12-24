6d6446cfbe1aa405fde1a2fa5fa1b5938b7ff25d
gde82 <1549599+gde82@users.noreply.github.com>
2021-12-24 11:47:51 +0100

    forgot parenthese on check git run script

--- run_once_install-liquidprompt.sh
+++ run_once_install-liquidprompt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ ! -d "~/.local/liquidprompt" && command -v git ]]
+if [[ ! -d "~/.local/liquidprompt" && "command -v git" ]]
     then
         mkdir -p ~/.local/
         git clone --branch stable https://github.com/nojhan/liquidprompt.git ~/.local/liquidprompt 
